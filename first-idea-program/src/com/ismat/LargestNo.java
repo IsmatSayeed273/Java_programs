e9be66e2f9ccea612144d0507b0159c8920e64bb
IsmatSayeed273 <ismat,sayeed273@gmail.com>
2025-08-10 21:27:32 +0530

    updated largest no

--- first-idea-program/src/com/ismat/LargestNo.java
+++ first-idea-program/src/com/ismat/LargestNo.java
@@ -6,18 +6,18 @@ public class LargestNo {
     public static void main(String[] args) {
         System.out.println("enter a: ");
         Scanner input = new Scanner(System.in);
-        int a = input.nextInt();
+        int p = input.nextInt();
         System.out.println("enter b: ");
-        int b = input.nextInt();
+        int q = input.nextInt();
         System.out.println("enter c: ");
-        int c = input.nextInt();
+        int r = input.nextInt();
 
-        if (a > b && a > c) {
-            System.out.println(a + " is the largest");
-        } else if (b > a && b > c) {
-            System.out.println(b + " is the largest");
+        if (p > q && p > r) {
+            System.out.println(p + " is the largest");
+        } else if (q > p && q > r) {
+            System.out.println(q + " is the largest");
         } else {
-            System.out.println(c + "is the largest");
+            System.out.println(r + "is the largest");
         }
     }
 }
